3d603e2e7670b9ea1674a4bc175dbf7acb273f0b
huyquang0112 <huyquang880@gmail.com>
2023-11-22 12:36:10 +0700

    update 22/11/2023

--- src/main/java/hotel/controller/BookingController.java
+++ src/main/java/hotel/controller/BookingController.java
@@ -12,12 +12,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import hotel.model.Account;
 import hotel.model.Booking;
@@ -47,30 +42,32 @@ public class BookingController {
 	}
 
 //	đặt phòng theo id
-	@GetMapping("/{id}") // xử lý yêu cầu HTTP trên đường dẫn "/booking/{id}"
-	public String bookingForm(Model model, 
-			@PathVariable("id") Long id, 
-			@ModelAttribute("currentRoom") Room room) {
-		Booking booking = new Booking();
-		Room room2 = roomRepo.findById(id).orElse(null); 
-		if (room2 != null) { 
-			room.setId(room2.getId());
-			room.setName(room2.getName());
-			room.setPrice(room2.getPrice());
-			room.setType(room2.getType());
-			room.setDescription(room2.getDescription());
-			room.setImage(room2.getImage());
-		}
-		model.addAttribute("room", room2);
-		model.addAttribute("booking", booking);
-		return "bookingInfo"; 
+@GetMapping("/{id}") // xử lý yêu cầu HTTP trên đường dẫn "/booking/{id}"
+public String bookingForm(Model model,
+						  @PathVariable("id") Long id,
+						  @ModelAttribute("currentRoom") Room room) {
+	Booking booking = new Booking();
+	Room room2 = roomRepo.findById(id).orElse(null);
+	if (room2 != null) {
+		room.setId(room2.getId());
+		room.setName(room2.getName());
+		room.setPrice(room2.getPrice());
+		room.setType(room2.getType());
+		room.setDescription(room2.getDescription());
+		room.setImage(room2.getImage());
 	}
+	model.addAttribute("room", room2);
+	model.addAttribute("booking", booking);
+	return "bookingInfo";
+}
 
-	@PostMapping // xử lý data gửi đến trên đường dẫn "/booking" 
-	public String createBooking(Model model, Booking currentBooking, HttpSession session) throws ParseException {
+	@PostMapping // xử lý data gửi đến trên đường dẫn "/booking"
+	public String createBooking(Model model, Booking currentBooking, HttpSession session, @RequestParam(name = "floor") Long floor) throws ParseException {
 //		lấy thông tin  từ session 
 		Room room = (Room) session.getAttribute("currentRoom");
+		System.out.println("Floor: " + floor);
 		room.setStatus("Hết");
+		room.setFloor(floor);
 		roomRepo.save(room);
 		Account account = (Account) session.getAttribute("currentAccount");
 		SimpleDateFormat fomatter = new SimpleDateFormat("yyyy-MM-dd");
@@ -85,15 +82,15 @@ public class BookingController {
 		Client client = clientRepo.findByUser(account.getUser()).orElse(null);
 		currentBooking.setRoom(room);
 		currentBooking.setClient(client);
-		long diff = datePayment.getTime() - dateReceipt.getTime(); 
+		long diff = datePayment.getTime() - dateReceipt.getTime();
 		// chuyển đổi sang số ngày
-		int totalDays = (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); 
+		int totalDays = (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 		currentBooking.setTotalPrice(room.getPrice()*totalDays);
 		currentBooking.setReceive(false);
 		currentBooking.setCancelled(false);
 		currentBooking.setPaid(false);
 		bookingRepo.save(currentBooking);
-		return "redirect:/room";  
+		return "redirect:/room";
 	}
 
 	@GetMapping("/list") // xem danh sách các phòng đã đặt
@@ -107,15 +104,15 @@ public class BookingController {
 	}
 
 //	lấy danh sách các phòng đã đặt mà chưa hủy
-	private List<Booking> filterByCancel(List<Booking> bookings) {
-		List<Booking> list = new ArrayList<>();
-		for (Booking booking : bookings) {
-			if (booking.isCancelled() == false) {
-				list.add(booking);
-			}
+private List<Booking> filterByCancel(List<Booking> bookings) {
+	List<Booking> list = new ArrayList<>();
+	for (Booking booking : bookings) {
+		if (booking.isCancelled() == false) {
+			list.add(booking);
 		}
-		return list;
 	}
+	return list;
+}
 
 	@GetMapping("/cancel/{id}") // xử lý yêu cầu hủy phòng rồi về trang danh sách phòng đã thuê
 	public String cancelBooking(@PathVariable("id") Long id, HttpSession session) {
@@ -129,5 +126,5 @@ public class BookingController {
 		}
 		return "redirect:/booking/list";
 	}
-	
+
 }
